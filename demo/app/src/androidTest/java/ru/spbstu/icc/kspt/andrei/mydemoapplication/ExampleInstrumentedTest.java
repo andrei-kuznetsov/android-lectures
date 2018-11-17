05cddeac1e94796a4e1931fcc8589b728da40b33
Andrei N. Kuznetsov <icc2013.android@gmail.com>
2018-11-17 23:02:06 +0300

    Remove premature androidx dependency (unwanted artifacts from future lecture)

--- demo/app/src/androidTest/java/ru/spbstu/icc/kspt/andrei/mydemoapplication/ExampleInstrumentedTest.java
+++ demo/app/src/androidTest/java/ru/spbstu/icc/kspt/andrei/mydemoapplication/ExampleInstrumentedTest.java
@@ -1,24 +1,13 @@
 package ru.spbstu.icc.kspt.andrei.mydemoapplication;
 
+import android.app.ListActivity;
 import android.os.RemoteException;
+import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import androidx.test.espresso.Espresso;
-import androidx.test.espresso.assertion.ViewAssertions;
-import androidx.test.espresso.matcher.ViewMatchers;
-import androidx.test.rule.ActivityTestRule;
-import androidx.test.runner.AndroidJUnit4;
-import androidx.test.uiautomator.UiDevice;
-
-import static androidx.test.espresso.Espresso.onData;
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.isRoot;
-import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static org.hamcrest.Matchers.is;
 
 /**
@@ -28,23 +17,9 @@ import static org.hamcrest.Matchers.is;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Rule
-    public ActivityTestRule<ListActivity> mActivityRule
-            = new ActivityTestRule<>(ListActivity.class);
 
     @Test
     public void rotationTest() throws InterruptedException, RemoteException {
-        UiDevice device = UiDevice.getInstance(getInstrumentation());
-        device.setOrientationNatural();
 
-        onView(ViewMatchers.withId(R.id.list_fragment)).check(ViewAssertions.matches(isDisplayed()));
-        onData(is("Item1")).perform(click());
-
-        onView(ViewMatchers.withId(R.id.detailsText)).check(ViewAssertions.matches(ViewMatchers.withText("Item1")));
-
-        device.setOrientationLeft();
-
-        getInstrumentation().waitForIdleSync();
-        onView(ViewMatchers.withId(R.id.list_fragment)).check(ViewAssertions.matches(isDisplayed()));
     }
 }
