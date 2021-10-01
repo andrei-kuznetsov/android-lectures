83488d1d9798e7e2208477bbd5e38457aa8d3904
Andrei Kuznetsov <andrei.kuznetsov@jetbrains.com>
2021-10-01 19:45:38 +0300

    continueWatch: обновлены версии gradle, agp, etc.

--- labs/02/continuewatch/app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
+++ labs/02/continuewatch/app/src/main/java/ru/spbstu/icc/kspt/lab2/continuewatch/MainActivity.kt
@@ -1,11 +1,12 @@
 package ru.spbstu.icc.kspt.lab2.continuewatch
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     var secondsElapsed: Int = 0
+    lateinit var textSecondsElapsed: TextView
 
     var backgroundThread = Thread {
         while (true) {
@@ -19,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textSecondsElapsed = findViewById(R.id.textSecondsElapsed)
         backgroundThread.start()
     }
 }
